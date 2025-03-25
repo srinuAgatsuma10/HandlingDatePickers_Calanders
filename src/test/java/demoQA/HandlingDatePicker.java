13626803480d3bb3c87c7d004c63f1f769920a9e
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-03-25 19:16:19 +0530

    demoQA final commit

--- src/test/java/demoQA/HandlingDatePicker.java
+++ src/test/java/demoQA/HandlingDatePicker.java
@@ -1,11 +1,13 @@
 package demoQA;
 
 import java.time.Duration;
+import java.util.*;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -20,7 +22,7 @@ public class HandlingDatePicker {
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://demoqa.com/date-picker");
-//		driver.manage().window().maximize();
+		driver.manage().window().maximize();
 	}
 
 	@Test
@@ -32,6 +34,27 @@ public class HandlingDatePicker {
 
 	@Test
 	public void usingCalander() {
+
+		String reqMonth = "Agust";
+		String reqYear = "2020";
+		String reqDate = "15";
+		// Click on datePicker
+		driver.findElement(By.xpath("//input[@id='datePickerMonthYearInput']")).click();
+		// Select Month and Year
+		WebElement monthDrp = driver.findElement(By.xpath("//select[@class='react-datepicker__month-select']"));
+		WebElement yearDrp = driver.findElement(By.xpath("//select[@class='react-datepicker__year-select']"));
+		Select month = new Select(monthDrp);
+		month.selectByVisibleText(reqMonth);
+		Select year = new Select(yearDrp);
+		year.selectByVisibleText(reqYear);
+		// Select Date
+		List<WebElement> allDates = driver.findElements(By.xpath("//div[@class='react-datepicker__month']//div"));
+		for (WebElement ad : allDates) {
+			if (ad.getText().equals(reqDate)) {
+				ad.click();
+				break;
+			}
+		}
 
 	}
 
