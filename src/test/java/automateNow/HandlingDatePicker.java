50193d99faf3f833f8a8167e2b9e4db3b0a05f01
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-03-31 20:34:37 +0530

    automateNow final commit

--- src/test/java/automateNow/HandlingDatePicker.java
+++ src/test/java/automateNow/HandlingDatePicker.java
@@ -22,35 +22,45 @@ public class HandlingDatePicker {
 		driver.get("https://practice-automation.com/calendars/");
 		driver.manage().window().maximize();
 	}
-	
+
 	@Test(description = "Handling Date Pickers")
 	public void datePicker() {
 		driver.findElement(By.xpath("//input[@id='g1065-2-1-selectorenteradate']")).click();
-		
+
 		String reqDate = "23";
 		String reqMonth = "August";
 		String reqYear = "2010";
-		
-		
-		while(true) {
+
+		selectMonthYear(driver, reqMonth, reqYear);
+		selectDate(driver, reqDate);
+
+		driver.findElement(
+				By.xpath("//div[@id='contact-form-1065-2-1']//button[@type='submit'][normalize-space()='Submit']"))
+				.click();
+
+	}
+
+	public static void selectMonthYear(WebDriver driver, String month, String year) {
+		while (true) {
 			String currentMonth = driver.findElement(By.xpath("//span[@class='ui-datepicker-month']")).getText();
 			String currentYear = driver.findElement(By.xpath("//span[@class='ui-datepicker-year']")).getText();
-			
-			if(currentYear.equals(reqYear) && currentMonth.equals(reqMonth)) {
+
+			if (currentYear.equals(year) && currentMonth.equals(month)) {
 				break;
 			}
-			driver.findElement(By.xpath("//a[@data-handler=\"prev\"]")).click();
-			
-			List<WebElement> allDates = driver.findElements(By.xpath("//td[@data-handler=\"selectDay\"]"));
-			for(WebElement ad : allDates) {
-				if(ad.getText().equals(reqDate)) {
-					ad.click();
-				}
+			driver.findElement(By.xpath("//a[@title='Prev']")).click();
+		}
+	}
+
+	public static void selectDate(WebDriver driver, String date) {
+		List<WebElement> allDates = driver.findElements(By.xpath("//td[@data-handler=\"selectDay\"]"));
+		for (WebElement ad : allDates) {
+			if (ad.getText().equals(date)) {
+				ad.click();
 			}
 		}
 	}
-	
-	
+
 	@AfterClass
 	public void tearDown() {
 		driver.quit();
