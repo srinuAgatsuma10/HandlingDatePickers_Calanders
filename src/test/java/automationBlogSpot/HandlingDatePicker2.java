7f43a7490a9bee94574818becde73116846a5341
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-07-15 12:41:30 +0530

    Implement Update

--- src/test/java/automationBlogSpot/HandlingDatePicker2.java
+++ src/test/java/automationBlogSpot/HandlingDatePicker2.java
@@ -1,9 +1,13 @@
 package automationBlogSpot;
 
 import java.time.Duration;
+import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -22,7 +26,31 @@ public class HandlingDatePicker2 {
 
 	@Test
 	public void datePicker() {
+		driver.findElement(By.xpath("//input[@id='txtDate']")).click();
 
+		String date = "29";
+		String month = "July";
+		String year = "2015";
+
+		WebElement monthDrpDown = driver.findElement(By.xpath("//select[@class='ui-datepicker-month']"));
+		WebElement yearDrpDown = driver.findElement(By.xpath("//select[@aclass='ui-datepicker-year']"));
+
+		Select sl1 = new Select(monthDrpDown);
+		sl1.selectByContainsVisibleText(month);
+		Select sl2 = new Select(yearDrpDown);
+		sl2.selectByContainsVisibleText(year);
+
+		selectDate(driver, date);
+
+	}
+
+	public static void selectDate(WebDriver driver, String date) {
+		List<WebElement> allDates = driver.findElements(By.xpath("//table[@class='ui-datepicker-calendar']//tr/td"));
+		for (WebElement ad : allDates) {
+			if (ad.getText().equals(date)) {
+				ad.click();
+			}
+		}
 	}
 
 	@AfterClass
